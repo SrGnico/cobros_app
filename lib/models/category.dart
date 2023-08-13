af5f9b3a4bed05f13d1bc4ed3ee09ea5bb0fcfa7
SrGnico <srgnicor@gmail.com>
2023-08-13 13:43:37 -0300

    better folder distribution

---
+++ lib/models/category.dart
@@ -0,0 +1,7 @@
+export 'category/category_colors.dart';
+export 'category/category_icons.dart';
+class Category{
+
+  static final List<String> categorias = ['Almacen', 'Lacteos', 'Congelados', 'Bebidas', 'Limpieza', 'Perfumeria','Suelto'];
+
+}
