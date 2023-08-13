794e03f54feae2e70150d1757b11164c4699397b
SrGnico <srgnicor@gmail.com>
2023-08-13 15:28:24 -0300

    implemented

---
+++ lib/widgets/record/record_list.dart
@@ -0,0 +1,43 @@
+import 'package:cobros_app/models/cart.dart';
+import 'package:flutter/material.dart';
+
+class RecordList extends StatelessWidget {
+
+  final Future<List<Cart>?> list;
+
+  const RecordList({
+    super.key,
+    required this.list
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 20),
+        Expanded(
+          child: FutureBuilder(
+            future: list,
+            builder: (context,AsyncSnapshot<List<Cart>?> snapshot){
+              if(snapshot.connectionState == ConnectionState.waiting){
+                return const Center(child: CircularProgressIndicator());
+              }
+              if(snapshot.hasData){
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    final item = snapshot.data![index];
+                    //Todo implement item to return
+                    return;
+                  },
+                );
+              }
+              else{return const Center(child: Text('No se han encontrado resultados!'));}
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
