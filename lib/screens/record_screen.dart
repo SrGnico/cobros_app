cb62a68e5752ca1e87675d972efe7b06110f24f2
SrGnico <srgnicor@gmail.com>
2023-08-17 16:48:30 -0300

    search by date, method implemented

--- lib/screens/record_screen.dart
+++ lib/screens/record_screen.dart
@@ -15,27 +15,56 @@ class _RecordScreenState extends State<RecordScreen> {
 
 
   Future<List<Cart>?> list = DatabaseRecord.getAllRecords();
+  DateTime selectedDate = DateTime.now();
+  String title = 'Todo';
+  
+  _selectDate(BuildContext context) async {
+  final DateTime? picked = await showDatePicker(
+    context: context,
+    initialDate: selectedDate, 
+    firstDate: DateTime(2023),
+    lastDate: DateTime(2030),
+    
+  );
+  if (picked != null){
+    setState(() {
+      String selectdDay = picked.day.toString();
+      String selectdMonth = picked.month.toString();
+      String selectdYear = picked.year.toString();
+
+      String searchDate = '$selectdDay/$selectdMonth/$selectdYear';
+      list = DatabaseRecord.getRecordByDate(searchDate);
+      title = searchDate;
+    });
+  }
+  if(picked == null){
+    setState(() {
+      title = 'Todo';
+      list = DatabaseRecord.getAllRecords();
+    });
+  }
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 150,
-        title: const Column(
+        title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               'Registro',
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.w300,
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Text(
-              'Todos',
+              title,
               maxLines: 1,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 80,
                 fontWeight: FontWeight.w500
               ),
@@ -47,22 +76,11 @@ class _RecordScreenState extends State<RecordScreen> {
             padding: const EdgeInsets.fromLTRB(0,50,10,0),
             child:IconButton(
               style: IconButton.styleFrom(backgroundColor: Colors.teal),
-              onPressed: () => showDialog(
-                context: context,
-                //todo Funciona esto? no se como handlear el input
-                builder: (context) => DatePickerDialog(
-                   initialDate: DateTime.now(),
-                    firstDate: DateTime(2023),
-                    lastDate: DateTime(2030),
-                    cancelText: 'Cancelar',
-                    confirmText: 'Listo',
-                    helpText: 'Buscar por fecha',
-                  ),
-              ),
+              onPressed: () => _selectDate(context),
               icon: const Padding(
                 padding: EdgeInsets.all(5.0),
                 child: Icon(
-                  Icons.sort_by_alpha_rounded,
+                  Icons.calendar_month_rounded,
                   color: Colors.white,
                   size: 65,
 
